52c7917b12fab453c151d33ca9464fa76c260a86
Rodolfo Ferro <rodolfoferroperez@gmail.com>
2019-10-24 09:05:16 -0500

    Initial commit. :rocket:

---
+++ code/iris.py
@@ -0,0 +1,26 @@
+# ===============================================================
+# Author: Rodolfo Ferro
+# Email: ferro@cimat.mx
+# Twitter: @FerroRodolfo
+#
+# ABOUT COPYING OR USING PARTIAL INFORMATION:
+# This script was originally created by Rodolfo Ferro,
+# for his workshop at RIIAA 2.0. Any explicit usage of
+# this script or its contents is granted according to
+# the license provided and its conditions.
+# ===============================================================
+
+# -*- coding: utf-8 -*-
+
+from tensorflow.keras.models import model_from_json
+
+
+def iris_classifier(verbose=True):
+    """Iris classifier trained model loader."""
+
+    model = None
+    return model
+
+
+if __name__ == '__main__':
+    model = iris_classifier()
